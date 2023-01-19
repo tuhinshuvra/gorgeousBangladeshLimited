a1d969cddb07ffab9f5eb8c3180b2f334c2bbeb7
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-19 15:05:37 +0600

    updated services section

--- src/Pages/BusinessSegment/BusinessSegment.js
+++ src/Pages/BusinessSegment/BusinessSegment.js
@@ -1,40 +1,110 @@
 import React from 'react';
+import AgroAnddFoods from '../../assets/images/categories/Agro_and_Foods.png';
+import CementIndustryMaterials from '../../assets/images/categories/Cement_Industry_Materials.png';
+import CommercialTransport from '../../assets/images/categories/Commercial_Transport.png';
+import LogisticsAndSecurity from '../../assets/images/categories/Logistics_and_Security.png';
+import PowerSolution from '../../assets/images/categories/Power_Solution.png';
+import RMGSector from '../../assets/images/categories/RMG_Sector.png';
+import { Link } from 'react-router-dom';
+import './Segment.css'
 
 const BusinessSegment = () => {
     return (
-        <div>
-            <h2>This Service Page</h2>
+        <div className=''>
+            <h1 className=' fw-bold mb-5 section-title'>Awesome <br /> Business Segment</h1>
 
             <div className=' row'>
-                <div className=' col-lg-4 col-2'>
-                    <div className="card mb-3" style={{}}>
-                        <div className="row g-0">
-                            <div className="col-md-6">
-                                <img src="..." className="img-fluid rounded-start" alt="..." />
+                <div className='segment'>
+                    <div class="card mb-3 border-0 " >
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src={AgroAnddFoods} class="img-fluid rounded-5 " alt="..." />
                             </div>
-                            <div className="col-md-6">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title float-start">AgroAnddFoods</h5>
+                                    <p class="card-text float-start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <Link><p class="card-text float-start fw-bold my-lg-4">More Details</p></Link>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="card mb-3" style={{}}>
-                        <div className="row g-0">
-                            <div className="col-md-6">
-                                <img src="..." className="img-fluid rounded-start" alt="..." />
+
+                    <div class="card mb-3 border-0 " >
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src={CementIndustryMaterials} class="img-fluid rounded-5" alt="..." />
                             </div>
-                            <div className="col-md-6">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title float-start">CementIndustryMaterials</h5>
+                                    <p class="card-text float-start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <Link><p class="card-text float-start fw-bold my-lg-4">More Details</p></Link>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <div class="card mb-3 border-0 " >
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src={CommercialTransport} class="img-fluid rounded-5" alt="..." />
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title float-start">CommercialTransport</h5>
+                                    <p class="card-text float-start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <Link><p class="card-text float-start fw-bold my-lg-4">More Details</p></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 border-0 " >
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src={LogisticsAndSecurity} class="img-fluid rounded-5" alt="..." />
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title float-start">LogisticsAndSecurity</h5>
+                                    <p class="card-text float-start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <Link><p class="card-text float-start fw-bold my-lg-4">More Details</p></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 border-0 " >
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src={PowerSolution} class="img-fluid rounded-5" alt="..." />
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title float-start">PowerSolution</h5>
+                                    <p class="card-text float-start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <Link><p class="card-text float-start fw-bold my-lg-4">More Details</p></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 border-0 " >
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src={RMGSector} class="img-fluid rounded-5" alt="..." />
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title float-start">RMGSector</h5>
+                                    <p class="card-text float-start">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <Link><p class="card-text float-start fw-bold my-lg-4">More Details</p></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Data from "./Data";
 import Card from "./Card";
 import Buttons from "./Buttons";
+import '../BusinessSegment/Segment.css';
 
 const App = () => {
     const [item, setItem] = useState(Data);
@@ -19,7 +20,7 @@ const App = () => {
         <>
             <div className="container-fluid">
                 <div className="row">
-                    <h1 className="col-12 text-center my-3 fw-bold">Our Products <br /> And Sevices</h1>
+                    <h1 className="col-12 text-center fw-bold section-title">Our Products <br /> And Sevices</h1>
                     <Buttons
                         filterItem={filterItem}
                         setItem={setItem}
