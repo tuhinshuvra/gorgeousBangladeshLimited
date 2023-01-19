fea10ece71cbc2f24a103f94f8ab2b0597ed5798
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-19 15:11:43 +0600

    added experience  section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Products from '../Products/Products';
 import BusinessSegment from '../BusinessSegment/BusinessSegment';
+import Experience from '../Experience/Experience';
 
 const Home = () => {
     return (
         <div>
+            <Experience></Experience>
             <BusinessSegment></BusinessSegment>
             <Products></Products>
 
