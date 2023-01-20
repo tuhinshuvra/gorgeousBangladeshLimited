bb52fc9001ec25fe9f9880763a616a4ed490ae01
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-20 15:25:22 +0600

    updated card image display size

--- src/Pages/Products/Card.js
+++ src/Pages/Products/Card.js
@@ -4,19 +4,22 @@ const Card = ({ item }) => {
 
     return (
         <>
-            <div className="container-fluid">
-                <div className="row justify-content-center">
-                    {item.map((Val) => {
-                        return (
-                            <div className="col-lg-3  my-2 py-2 border-0 rounded-5" key={Val.id}>
-                                <div className="col-lg-10">
-                                    <div className="">
-                                        <img src={Val.img} alt={Val.title} className=" img-fluid rounded-3 " />
+            <div className="">
+                <div className="row">
+                    <div className="product my-3">
+
+                        {item.map((Val) => {
+                            return (
+                                <div className="  " key={Val.id}>
+                                    <div className=" col-lg-12 ">
+                                        <div className="">
+                                            <img src={Val.img} alt={Val.title} className=" img-fluid rounded-4 " />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        );
-                    })}
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </>
